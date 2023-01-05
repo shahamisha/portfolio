9189bda2f78f4b45b5ce4e8ee002f7fbcdc2ef76
shahamisha <shahamisha012@gmail.com>
2023-01-05 19:26:28 +0530

    commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,27 @@
+import Navbar from "./sections/navbar/Navbar";
+import Header from "./sections/header/Header";
+import About from "./sections/about/About";
+import Services from "./sections/services/Services";
+import Portfolio from "./sections/portfolio/Portfolio";
+import Testimonials from "./sections/testimonials/Testimonials";
+import FAQS from "./sections/faqs/FAQS";
+import Contact from "./sections/contact/Contact";
+import Footer from "./sections/footer/Footer";
+
 const App = () => {
-  return <div>Hello World</div>;
+  return (
+    <main>
+      <Navbar />
+      <Header />
+      <About />
+      <Services />
+      <Portfolio />
+      <Testimonials />
+      <FAQS />
+      <Contact />
+      <Footer />
+    </main>
+  );
 };
 
 export default App;
