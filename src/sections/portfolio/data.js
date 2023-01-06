27f804a30b1111cc024a0f6670f1b49f6bb8d30b
shahamisha <shahamisha012@gmail.com>
2023-01-06 23:43:54 +0530

    portfolio

---
+++ src/sections/portfolio/data.js
@@ -0,0 +1,86 @@
+import Image1 from "../../assets/pictures1.jpeg";
+import Image2 from "../../assets/pictures2.jpeg";
+import Image3 from "../../assets/pictures3.jpeg";
+import Image4 from "../../assets/pictures4.jpeg";
+import Image5 from "../../assets/pictures5.jpeg";
+import Image6 from "../../assets/pictures6.jpeg";
+import Image7 from "../../assets/pictures7.jpeg";
+import Image8 from "../../assets/pictures8.jpeg";
+import Image9 from "../../assets/pictures9.jpeg";
+
+const data = [
+  {
+    id: 1,
+    category: "uiux",
+    image: Image1,
+    title: "Project Title One (UIUX)",
+    desc: "Provident similique accusantium nemo autem. Veritatis obcaecati tenetur iure eius earum ut molestias architecto voluptate aliquam nihil, eveniet aliquid culpa officia aut!",
+    github: "https://github.com/shahamisha",
+  },
+  {
+    id: 2,
+    category: "frontend",
+    image: Image2,
+    title: "Project Title Two (Frontend)",
+    desc: "Provident similique accusantium nemo autem. Veritatis obcaecati tenetur iure eius earum ut molestias architecto voluptate aliquam nihil, eveniet aliquid culpa officia aut!",
+    github: "https://github.com/shahamisha",
+  },
+  {
+    id: 3,
+    category: "frontend",
+    image: Image3,
+    title: "Project Title Three (Frontend)",
+    desc: "Provident similique accusantium nemo autem. Veritatis obcaecati tenetur iure eius earum ut molestias architecto voluptate aliquam nihil, eveniet aliquid culpa officia aut!",
+    github: "https://github.com/shahamisha",
+  },
+  {
+    id: 4,
+    category: "uiux",
+    image: Image4,
+    title: "Project Title Four (UIUX)",
+    desc: "Provident similique accusantium nemo autem. Veritatis obcaecati tenetur iure eius earum ut molestias architecto voluptate aliquam nihil, eveniet aliquid culpa officia aut!",
+    github: "https://github.com/shahamisha",
+  },
+  {
+    id: 5,
+    category: "uiux",
+    image: Image5,
+    title: "Project Title Five (UIUX)",
+    desc: "Provident similique accusantium nemo autem. Veritatis obcaecati tenetur iure eius earum ut molestias architecto voluptate aliquam nihil, eveniet aliquid culpa officia aut!",
+    github: "https://github.com/shahamisha",
+  },
+  {
+    id: 6,
+    category: "frontend",
+    image: Image6,
+    title: "Project Title Six (Frontend)",
+    desc: "Provident similique accusantium nemo autem. Veritatis obcaecati tenetur iure eius earum ut molestias architecto voluptate aliquam nihil, eveniet aliquid culpa officia aut!",
+    github: "https://github.com/shahamisha",
+  },
+  {
+    id: 7,
+    category: "frontend",
+    image: Image7,
+    title: "Project Title Seven (Frontend)",
+    desc: "Provident similique accusantium nemo autem. Veritatis obcaecati tenetur iure eius earum ut molestias architecto voluptate aliquam nihil, eveniet aliquid culpa officia aut!",
+    github: "https://github.com/shahamisha",
+  },
+  {
+    id: 8,
+    category: "backend",
+    image: Image8,
+    title: "Project Title Eight (Backend)",
+    desc: "Provident similique accusantium nemo autem. Veritatis obcaecati tenetur iure eius earum ut molestias architecto voluptate aliquam nihil, eveniet aliquid culpa officia aut!",
+    github: "https://github.com/shahamisha",
+  },
+  {
+    id: 9,
+    category: "frontend",
+    image: Image9,
+    title: "Project Title Nine (Frontend)",
+    desc: "Provident similique accusantium nemo autem. Veritatis obcaecati tenetur iure eius earum ut molestias architecto voluptate aliquam nihil, eveniet aliquid culpa officia aut!",
+    github: "https://github.com/shahamisha",
+  },
+];
+
+export default data;
