2121159375bf226b2d323bb382112d77552be5ad
shahamisha <shahamisha012@gmail.com>
2023-01-08 00:27:19 +0530

    faqs

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import About from "./sections/about/About";
 import Services from "./sections/services/Services";
 import Portfolio from "./sections/portfolio/Portfolio";
 import Testimonials from "./sections/testimonials/Testimonials";
-import FAQS from "./sections/faqs/FAQS";
+import FAQs from "./sections/faqs/FAQs";
 import Contact from "./sections/contact/Contact";
 import Footer from "./sections/footer/Footer";
 
@@ -17,7 +17,7 @@ const App = () => {
       <Services />
       <Portfolio />
       <Testimonials />
-      <FAQS />
+      <FAQs />
       <Contact />
       <Footer />
     </main>
