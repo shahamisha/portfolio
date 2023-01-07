41cdf0babefd41e516959e32ebce7d3785825ab3
shahamisha <shahamisha012@gmail.com>
2023-01-07 17:16:49 +0530

    commit

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -2,7 +2,7 @@ import "./card.css";
 
 const Card = ({ children, className, onClick }) => {
   return (
-    <article className={`card ${className}`} onclick={onClick}>
+    <article className={`card ${className}`} onClick={onClick}>
       {children}
     </article>
   );
