f0233098dd8d5b73f1a4d7f0da49d899634372d9
shahamisha <shahamisha012@gmail.com>
2023-01-08 01:37:53 +0530

    contact

---
+++ src/sections/contact/data.js
@@ -0,0 +1,15 @@
+import { HiOutlineMail } from "react-icons/hi";
+import { RiMessengerLine } from "react-icons/ri";
+import { FaWhatsapp } from "react-icons/fa";
+
+const data = [
+  { id: 1, icon: <HiOutlineMail />, link: "mailto:shahamisha012@gmail.com" },
+  {
+    id: 2,
+    icon: <RiMessengerLine />,
+    link: "https://www.facebook.com/amisha0012?mibextid=ZbWKwL",
+  },
+  { id: 3, icon: <FaWhatsapp />, link: "https://wa.me/9825955473" },
+];
+
+export default data;
