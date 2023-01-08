cbbedcff7a8877f79e13eeca5be28572aa874b58
shahamisha <shahamisha012@gmail.com>
2023-01-08 17:52:47 +0530

    footer

---
+++ src/sections/footer/data.js
@@ -0,0 +1,25 @@
+import { AiOutlineInstagram } from "react-icons/ai";
+import { AiOutlineTwitter } from "react-icons/ai";
+import { AiFillGithub } from "react-icons/ai";
+
+export const links = [
+  { id: 1, link: "#", title: "Home" },
+  { id: 2, link: "#about", title: "About" },
+  { id: 3, link: "#services", title: "Services" },
+  { id: 4, link: "#portfolio", title: "Portfolio" },
+  { id: 5, link: "#contact", title: "Contact" },
+];
+
+export const socials = [
+  {
+    id: 1,
+    link: "https://www.instagram.com/amisha9813",
+    icon: <AiOutlineInstagram />,
+  },
+  {
+    id: 1,
+    link: "https://twitter.com/shahamisha012",
+    icon: <AiOutlineTwitter />,
+  },
+  { id: 1, link: "https://github.com/shahamisha", icon: <AiFillGithub /> },
+];
